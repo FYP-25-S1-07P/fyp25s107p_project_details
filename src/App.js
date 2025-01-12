c31cfacb409e88da597a2a6b0af8ec4fd31a3cf5
Victor Jong <victorjongsoon@hotmail.com>
2025-01-12 11:16:46 +0800

    initial commit with react app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to FYP-25-S1-07P Project Website</h1>
+      <p>This is our project details website built with React!</p>
+    </div>
+  );
+}
+
+export default App;
